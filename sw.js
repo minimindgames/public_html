1fba0be1650ed3607ba23715f445761ebf0a4ddd
minimindgames <info@minimindgames.com>
2021-02-17 18:49:29 +0200

    Enable virtual pad

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init({
     assetCachingOptions: [{
-            regexp: /\.(png|jpg|woff2|woff|css|js)/,
+            regexp: /\.(png|jpg|mp3|ogg|css|js)/,
             cachingStrategy: 'CACHE_FIRST'
         }],
     offlinePageOptions: {
-        url: '/index.html',
-        assets: []
+        url: '/offline.html',
+        assets: ['/img/bot.jpg']
     }
 });
